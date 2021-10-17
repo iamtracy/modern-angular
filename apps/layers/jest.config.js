d6ce23b82801376e2765068d3bfd328698471628
Tracy - Personal <tracy-personal@Tracys-MBP.lan>
2021-10-17 03:29:32 -0400

    initial commit

---
+++ apps/layers/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'layers',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/layers',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
